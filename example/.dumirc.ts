622d9528a494bac5e23b6b70f5776fc33e511233
wxik <stulip@126.com>
2023-06-23 20:37:49 +0800

    feat: github build

--- example/.dumirc.ts
+++ example/.dumirc.ts
@@ -1,16 +1,19 @@
 import {defineConfig} from 'dumi';
 
 const path = require('path');
-
+const __DEV__ = process.env.NODE_ENV !== 'production';
+const repo = 'react-form';
 export default defineConfig({
   title: 'Reaction Form',
-  logo: '/reaction.svg',
+  logo: `${!__DEV__ ? `/${repo}` : ''}/reaction.svg`,
   favicons: ['/favicon.svg'],
   themeConfig: {
     footer: `Open-source MIT Licensed | Copyright © 2023-present <br/> Powered by self`,
     prefersColor: {default: 'light', switch: false},
   },
   outputPath: path.join(__dirname, '..', 'docs'),
+  base: !__DEV__ ? `/${repo}/` : '/',
+  publicPath: !__DEV__ ? `/${repo}/` : '/',
   alias: {
     '@wxik/react-form': path.resolve('../src'),
     '@': path.resolve(__dirname, 'src'),
